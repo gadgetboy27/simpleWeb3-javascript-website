bf19a9e346bc2615a5d1c546599fe0d987888e96
Henry-Peti <iamgadgetboy@gmail.com>
2020-05-08 05:32:39 +1200

    changed all products to modal and shadowbox form

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,7 +1,7 @@
 // Get DOM Elements
-const modal = document.querySelector('#prod-modal');
-const modalBtn = document.querySelector('#modalBtn');
-const closeBtn = document.getElementsByClassName('closeBtn')[0];
+const modal = document.querySelector('#prod-modal')
+const modalBtn = document.querySelector('#modalBtn')
+const closeBtn = document.getElementsByClassName('closeBtn')[0]
 
 // Events
 modalBtn.addEventListener('click', openModal)
@@ -10,17 +10,17 @@ window.addEventListener('click', outsideClick)
 
 // Open
 function openModal() {
-  modal.style.display = 'block';
+  modal.style.display = 'block'
 }
 
 // Close
 function closeModal() {
-  modal.style.display = 'none';
+  modal.style.display = 'none'
 }
 
 // Close If Outside Click
 function outsideClick(e) {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal.style.display = 'none'
   }
 }
